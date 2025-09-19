c182242899ab92388af29b873df4e9c3bef5181a
OrlandoDev17 <orlandodev1707@gmail.com>
2025-09-18 21:01:12 -0400

    ## [1.0.2] - 2025-09-18
    ### Added
    
    - 🛠️ Script postinstalación interactivo para verificar e instalar TailwindCSS
    - 📦 Mensaje de bienvenida al instalar VisuraUIAstro
    
    ### Changed
    
    - 🔧 Ajustes en `package.json` para incluir `postinstall.js`
    - 📚 Mejoras en documentación de instalación
    
    ### Fixed
    
    - 🐛 Corrección de rutas de exportación en `index.ts`

---
+++ astro/CHAGELOG.md
@@ -0,0 +1,21 @@
+## [1.0.1] - 2025-09-18
+
+### Cambios
+
+- Agregar mensajes para el usuario al instalar el paquete
+
+## [1.0.2] - 2025-09-18
+
+### Added
+
+- 🛠️ Script postinstalación interactivo para verificar e instalar TailwindCSS
+- 📦 Mensaje de bienvenida al instalar VisuraUIAstro
+
+### Changed
+
+- 🔧 Ajustes en `package.json` para incluir `postinstall.js`
+- 📚 Mejoras en documentación de instalación
+
+### Fixed
+
+- 🐛 Corrección de rutas de exportación en `index.ts`
--- astro/pointinstall.js
+++ astro/pointinstall.js
@@ -1,8 +1,38 @@
-console.log("\n🚀 Gracias por instalar VisuraUIAstro!");
-console.log(
-  "⚠️ Este paquete requiere TailwindCSS para funcionar correctamente."
-);
-console.log("👉 ¿No lo tenés instalado? Ejecutá:");
-console.log("\nnpm install tailwindcss @tailwindcss/vite");
-console.log("Luego crea un archivo global.css en src/styles");
-console.log('Y agrega esta linea @import "tailwindcss";');
+const prompts = require("prompts");
+const { execSync } = require("child_process");
+
+(async () => {
+  console.log("\n🚀 Gracias por instalar VisuraUIAstro!");
+  console.log(
+    "⚠️ Esta librería requiere TailwindCSS para funcionar correctamente.\n"
+  );
+
+  const response = await prompts({
+    type: "confirm",
+    name: "install",
+    message: "¿Querés que instalemos TailwindCSS por vos?",
+    initial: true,
+  });
+
+  if (response.install) {
+    console.log("\n📦 Instalando TailwindCSS...");
+    execSync("npm install -D tailwindcss @tailwindcss/vite", {
+      stdio: "inherit",
+    });
+    execSync("mkdir src/styles", { stdio: "inherit" });
+    execSync("touch src/styles/global.css", { stdio: "inherit" });
+    console.log("\n✅ TailwindCSS instalado y configurado.");
+  } else {
+    console.log(
+      "\n⚠️ Recordá instalarlo manualmente para que VisuraUI funcione correctamente."
+    );
+    console.log("👉 npm install -D tailwindcss @tailwindcss/vite");
+    console.log("👉 mkdir src/styles");
+    console.log("👉 touch src/styles/global.css\n");
+    console.log("👉 Agrega esta linea a global.css\n");
+    console.log("import @tailwindcss");
+    console.log(
+      "E importa ese archivo en el archivo en tu raiz Ej. src/main.ts o src/layout.astro\n"
+    );
+  }
+})();
